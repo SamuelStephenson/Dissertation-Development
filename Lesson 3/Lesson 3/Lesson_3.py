a2d30cf03bfa92ea0a49091aa6cc0ad7d3bd1409
SamuelStephenson <49656721+SamuelStephenson@users.noreply.github.com>
2019-04-30 12:21:06 +0100

    Add files via upload

---
+++ Lesson 3/Lesson 3/Lesson_3.py
@@ -0,0 +1,23 @@
+#l = [1,2,3,4,5]
+
+#print(l)
+
+#l[1] = 99
+#print(l)
+game = [[0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0],]
+
+game[0][1] = 1  #Modifies the list to change the value of the First interation to a value of 1
+print("   a  b  c")
+#count = 0
+#https://docs.python.org/3/library/functions.html 
+for count, row in enumerate (game):
+    print(count, row)
+   
+ game[0][1] = 1  
+    
+    
+    #count += 1
+  #for item in row:
+     # print(item)
---
+++ Lesson 3/Tik_Tak_Toe_Clean/Tik_Tak_Toe_Clean/Tik_Tak_Toe_Clean.py
@@ -0,0 +1,14 @@
+game =[[0,0,0],
+       [0,0,0],
+       [0,0,0],]
+
+print(" 0 1 2")
+for count, row in enumerate(game):
+    print(count, row)
+
+    [0][1] = 1
+
+    print("0 1 2")
+    for count, row, in enumerate(game):
+        print(count,row)
+     
